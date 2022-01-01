c77e0d731f80978e7719c2de70ffc868f71e98fc
Mukesh Kumar <mukeshk0326@gmail.com>
2022-01-01 12:26:16 +0530

    path changed

--- assignments/assignment_2/index.js
+++ assignments/assignment_2/index.js
@@ -1,26 +1,6 @@
-// var fs = require('fs');
-// let content = '<h1>Hello World</h1>';
-// fs.writeFileSync('index.html',content,(err)=>{
-//     console.log(err)
-// })
-// var newFile=fs.readFileSync("./index.html",{'Content-Type':"text/html"},content,(err)=>{
-//     console.log(err)
-// })
-// var http=require("http");
-// var server=http.createServer((req,res)=>{
-//     if(req.url=="/"){
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     res.write(newFile);
-//     res.end();
-//     }
-// }
-
-// )
-// server.listen(3000,'localhost')
-
 var fs = require("fs");
 fs.writeFileSync(
-  "./10x Node Assignments\10x-Node-Assignments\assignments\assignment_2/index.html",
+  "index.html",
   "<h1>Hello World</h1>",
   (err) => {
     if (err) console.log(err);
@@ -33,7 +13,7 @@ fs.writeFileSync(
 );
 const http = require("http");
 const newFile = fs.readFileSync(
-  "./10x Node Assignments\10x-Node-Assignments\assignments\assignment_2/index.html",
+  "index.html",
   (err, data) => {
     if (err) console.log(err);
     console.log(data);
